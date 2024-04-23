04c8ecd55d46d2230dc0f65ce298072112caace2
qridohginoga <163043560+qridohginogaa@users.noreply.github.com>
2024-04-23 21:48:54 +0800

    added file

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['CSE Student at NMIT ','Frontend developer'],
+    strings:['Universitas ','Samratulangi'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
