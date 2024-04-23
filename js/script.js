aad5941184133786a69f90a4a21925e5e085f574
qridohginoga <163043560+qridohginogaa@users.noreply.github.com>
2024-04-23 22:08:33 +0800

    added file

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Universitas Samratulangi ','Informatika'],
+    strings:['Teknik Informatika ','Universitas Samratulangi'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
